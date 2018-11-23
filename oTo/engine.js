3a8421bba32bd80b6c95d7b207a607405178d2bb
nypher <nypher@nypher.com>
2018-11-23 20:05:49 +0100

    Add files via upload

--- oTo/engine.js
+++ oTo/engine.js
@@ -2,23 +2,29 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-let oTo = new Proxy( {
+var oTo = new Proxy( {
+	// Set the path to oTo 
+	path: '/oTo',
+	// Parts folder name
+	partsPath: '/parts',
+	// Set an alias for the oTo object if set you will be able to use you customNamed object as well as oTo.
+	alias: false,
 	// List oTo dependencies, if any.
-	// For local resource, use the name of the file located under "/oTo/deps/" without the .js extension.
+	// For local resource, use the name of the file located under "/oTo/parts/" without the .js extension.
 	// For external sources, use the complete path including "http://" or "https://".
-	deps: [
+	dependencies: [
 		'jquery'
 	],
 	// Will be updated to support nodejs in the future
 	js : function(url, callback){
-		let h = document.getElementsByTagName("HEAD")[0];
-		let s = document.createElement('script');
+		var h = document.getElementsByTagName("HEAD")[0];
+		var s = document.createElement('script');
 		s.type = 'text/javascript';
 		s.src = url;
 		s.async = false;
 		h.appendChild(s);
 		s.onreadystatechange = s.onload = function() {
-			let state = s.readyState;
+			var state = s.readyState;
 			if (callback instanceof Function && !callback.done && (!state || /loaded|complete/.test(state))) {
 				callback.done = true;
 				callback();
@@ -27,14 +33,15 @@ let oTo = new Proxy( {
 	},
 	// Load dependencies first and the init.js file that contains the script using oTo.
 	engine: function(){
-		oTo.engine.state = 0;
-		for (var i = oTo.deps.length - 1; i >= 0; i--) {
-			let u = ( oTo.deps[i].indexOf("http://") == 0 || oTo.deps[i].indexOf("https://") == 0 ) ? oTo.deps[i] : '/oTo/deps/' + oTo.deps[i] + '.js';
-			oTo.js( u, function(){
-				oTo.engine.state++;
-				if(oTo.engine.state === oTo.deps.length){
-					// Call oTo.start located on the /oTo/parts/start.js file
-					oTo.log('This is a function from another file!')
+		var obj = oTo.alias?window[oTo.alias]:oTo;
+		obj.engine.state = 0;
+		for (var i = obj.dependencies.length - 1; i >= 0; i--) {
+			var u = ( obj.dependencies[i].indexOf("http://") == 0 || obj.dependencies[i].indexOf("https://") == 0 ) ? obj.dependencies[i] : obj.path + obj.partsPath + '/' + obj.dependencies[i] + '.js';
+			obj.js( u, function(){
+				obj.engine.state++;
+				if(obj.engine.state === obj.dependencies.length){
+					// Call obj.start located on the /oTo/parts/start.js file
+					obj.start()
 				}
 			});
 		}
@@ -42,17 +49,21 @@ let oTo = new Proxy( {
 },{
 	// Proxy that handle all oTo calls.
 	get : function(target, prop){
-		let timeStampInMs = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
+		if(oTo.alias){
+			window[oTo.alias] = oTo;
+		}
+		var timeStampInMs = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
 		if( prop in target ) {
             return target[prop];
 		}else{
 			return function (args) {
-				target.js( '/oTo/parts/' + prop + '.js', function(){
-					if( prop in oTo ) {
-						if(oTo[prop] instanceof Function){
-							oTo[prop](args);
+				target.js( target.path + target.partsPath + '/' + prop + '.js', function(){
+					var obj = oTo.alias?window[oTo.alias]:oTo;
+					if( prop in obj ) {
+						if(obj[prop] instanceof Function){
+							obj[prop](args);
 						}else{
-							return oTo[prop];
+							return obj[prop];
 						}
 					}else{
 						return false;
---
+++ oTo/parts/start.js
@@ -0,0 +1,4 @@
+// this is the initial script called when oTo is ready to be used.
+oTo.start = function() {
+	// Here you can execute anything
+};
