fbf29715fdb8fd171ee1ed85c3d51484b835dca7
Fernán García de Zúñiga <nypher@nypher.com>
2026-03-19 10:53:44 +0100

    Refactor oTo.log function with new license information
    Updated oTo.log function to include a new license comment.

--- oTo/parts/log.js
+++ oTo/parts/log.js
@@ -1,7 +1,4 @@
-// oTo.log - oTo Console Log Styles by nypher at github.com/nypher/oTo
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// oTo.log - Object Transport Object Log function to serve as an example of how to extend oTo by fernangz under license EUPL [https://eupl.eu/]
 oTo.log = function(content, style) {
 	var e = 'background: #fdd001; color: #c00000; line-height: 14px; font-size: 12px; padding: 8px; font-weight: 600;';
 	var a = 'background: #000; color: #fdd001; line-height: 14px; font-size: 12px; padding: 8px; font-weight: 600;';
